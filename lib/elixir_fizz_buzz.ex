c0b1fee22f9921cdcba86d32b529fb6dcf5f7619
Gabriel Santos Cardoso <eng.gabrielscardoso@gmail.com>
2022-12-30 00:18:18 -0300

    initial commit

---
+++ lib/elixir_fizz_buzz.ex
@@ -0,0 +1,18 @@
+defmodule ElixirFizzBuzz do
+  @moduledoc """
+  Documentation for `ElixirFizzBuzz`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ElixirFizzBuzz.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
