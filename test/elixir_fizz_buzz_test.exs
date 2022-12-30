f7a241f073df3215bfc3abd2993195d7afcf9fe3
Gabriel Santos Cardoso <eng.gabrielscardoso@gmail.com>
2022-12-30 01:33:41 -0300

    test: create test set for ElixirFizzBuzz module

--- test/elixir_fizz_buzz_test.exs
+++ test/elixir_fizz_buzz_test.exs
@@ -1,8 +1,20 @@
 defmodule ElixirFizzBuzzTest do
   use ExUnit.Case
-  doctest ElixirFizzBuzz
 
-  test "greets the world" do
-    assert ElixirFizzBuzz.hello() == :world
+  # We always should test public functions
+  # Format: <function_name>/<arity>
+  describe "build/1" do
+    test "when a valid file is provided, returns the converted list" do
+      expected_response = {:ok, [1, 2, :fizz, 4, :buzz, :fizz, 7, 8, :fizz, :buzz, 11, :fizz, 13, 14, :fizz_buzz]}
+
+      ## Set your absolute path to file numbers.txt
+      assert ElixirFizzBuzz.build("") == expected_response
+    end
+
+    test "when an invalid file is provided, returns an error" do
+      expected_response = {:error, "Error reading the file: enoent"}
+
+      assert ElixirFizzBuzz.build("invalid.txt") == expected_response
+    end
   end
 end
