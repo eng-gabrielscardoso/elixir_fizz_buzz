6610d82093152e252f96b0b7d896671ae3d7c37b
Gabriel Santos Cardoso <eng.gabrielscardoso@gmail.com>
2022-12-30 01:11:02 -0300

    refactor: enhance code with 'elixir way'

--- lib/elixir_fizz_buzz.ex
+++ lib/elixir_fizz_buzz.ex
@@ -5,22 +5,24 @@ defmodule ElixirFizzBuzz do
     handle_file_read()
   end
 
-  def handle_file_read({:ok, result}) do
-    result |>
-    String.split(", ") |>
-    Enum.map(&convert_and_evaluate_numbers/1)
+  defp handle_file_read({:ok, result}) do
+    result = result |>
+      String.split(", ") |>
+      Enum.map(&convert_and_evaluate_numbers/1)
+
+    {:ok, result}
   end
 
-  def handle_file_read({:error, reason}), do: "Error reading the file: #{reason}"
+  defp handle_file_read({:error, reason}), do: {:error, "Error reading the file: #{reason}"}
 
-  def convert_and_evaluate_numbers(numbers) do
+  defp convert_and_evaluate_numbers(numbers) do
     numbers |>
     String.to_integer() |>
     evaluate_number()
   end
 
-  def evaluate_number(number) when rem(number, 3) == 0 and rem(number, 5) == 0, do: :fizz_buzz
-  def evaluate_number(number) when rem(number, 5) == 0, do: :buzz
-  def evaluate_number(number) when rem(number, 3) == 0, do: :fizz
-  def evaluate_number(number), do: number
+  defp evaluate_number(number) when rem(number, 3) == 0 and rem(number, 5) == 0, do: :fizz_buzz
+  defp evaluate_number(number) when rem(number, 5) == 0, do: :buzz
+  defp evaluate_number(number) when rem(number, 3) == 0, do: :fizz
+  defp evaluate_number(number), do: number
 end
